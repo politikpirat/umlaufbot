ff9e21ce64a0b6852fa1fbfd1031f7132af6e975
Martin Delius <dls@Debian-70-wheezy-64-minimal>
2013-09-15 23:15:29 +0200

    working url extraction and checking for redundant posting. status-updates are tested but not yet implemented.

--- robot.py
+++ robot.py
@@ -1,8 +1,8 @@
 #coding: utf8 
 from twitter import *
-import os, time
+import os, time, re
 
-umbot = Twitter( auth=OAuth('1867015789-2rfQxS9h7wx4FegefewtTj4pjQzzuAkBYSSFy0e', 'LTHF2nEHoX8XXDKKNoDI3YkGfcFwVhKOzfuDzpfPgzg', 'W5XZIAFWNRlztuO2zhVggg', 'cdiB8jJ2YlSBCXx5nN5WeBStgLlDeeKOeR8Lmtvwfts'))
+umbot = Twitter(api_version='1.1', auth=OAuth('1867015789-2rfQxS9h7wx4FegefewtTj4pjQzzuAkBYSSFy0e', 'LTHF2nEHoX8XXDKKNoDI3YkGfcFwVhKOzfuDzpfPgzg', 'W5XZIAFWNRlztuO2zhVggg', 'cdiB8jJ2YlSBCXx5nN5WeBStgLlDeeKOeR8Lmtvwfts'))
 
 participants = "participants.list"
 last = "last.txt"
@@ -34,11 +34,18 @@ if str(id) == last_call:
 	print "there haven\'t been any new DMs yet."
 else: 
 	while True:
-		for n in range(len(members)):
-			text = '@{0} Von @{1} Wurde ein neuer Umlaufbeschluss für die #15Piraten eingestellt.'.format(members[n],dms[i]['sender_screen_name'])
-			print text
-			#umbot.statusses.update(status=text)
- 		i+=1
+		dmtext = dms[i]['text']		
+		if dmtext.find('http') != -1:
+			url = re.search('http[\S]*',dmtext)
+			for n in range(len(members)):
+				member = members[n].rstrip('\n')
+				if dms[i]['sender_screen_name'] != member:
+					text = '@{0} Von @{1} wurde ein neuer Umlaufbeschluss für die #15Piraten eingestellt. Schau hier nach: {2}'.format(member,dms[i]['sender_screen_name'],url.group())
+					print text
+					#umbot.statuses.update(status=text)
+		else:
+			print "there is no umlauf in this DM"
+		i+=1
 		id = dms[i]['id']
 		if str(id) == last_call:
 			break
@@ -46,7 +53,6 @@ else:
 if os.path.exists(last):
     	fp = open(last, 'w')
 	latest = str(dms[0]['id'])
-	print latest
 	fp.write(latest) 
     	fp.close()
 
